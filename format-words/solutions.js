b65fda34d5c96c0059f135604703288f94853595
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-01-22 08:31:24 -0800

    added solution to word format

---
+++ format-words/solutions.js
@@ -0,0 +1,12 @@
+function formatWords(words){
+    if(words === []){
+      return "";
+    }
+    
+    if(words.length === 2){
+      return words.join(" and ");
+    }
+    
+    if(words.length > 2){
+      return words.
+  }
